5a2df42ad58968c3b79c49031b01bd30271ecdef
Harshil Patel <120940967+harxxhilgg@users.noreply.github.com>
2023-05-28 00:25:20 +0530

    updated typography and styling

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-var initialPrice = document.querySelector('#initial-price');
-var stockQuantity = document.querySelector('#stocks-quantity');
-var currentPrice = document.querySelector('#current-price');
+var initialPrice = document.querySelector("#initialPrice");
+var stockQuantity = document.querySelector("#stocksQuantity");
+var currentPrice = document.querySelector("#currentPrice");
+var checkBtn = document.querySelector("#checkBtn");
 
-var checkBtn = document.querySelector('#checkBtn');
+checkBtn.addEventListener("click", () => {
+  var initialpce = Number(initialPrice.value);
+  var stockqntty = Number(stockQuantity.value);
+  var currentpce = Number(currentPrice.value);
 
-checkBtn.addEventListener('click', () => {
-    var initialpce = Number(initialPrice.value);
-    var stockqntty = Number(stockQuantity.value);
-    var currentpce = Number(currentPrice.value);
-
-    calculateProfit(initialpce, stockqntty, currentpce);
-})
+  calculateProfit(initialpce, stockqntty, currentpce);
+});
 
 function calculateProfit(initial, quantity, current) {
-    if ( initialPrice.value.length === 0 || currentPrice.value.length === 0 || stockQuantity.value.length === 0 ) {
-        outputHandler('PLEASE FILL OUT ALL FIELDS!');
-        outputBox.style.color = "red";
+  if (
+    initialPrice.value.length === 0 ||
+    currentPrice.value.length === 0 ||
+    stockQuantity.value.length === 0
+  ) {
+    outputHandler("PLEASE FILL OUT ALL FIELDS!");
+    outputBox.style.color = "red";
+  } else {
+    if (initial > current) {
+      var loss = (initial - current) * quantity;
+      var l_loss = loss.toFixed(2);
+      var lossPercent = (loss / initial) * 100;
+      var l_percent = lossPercent.toFixed(2);
+      outputHandler(
+        "absolute loss is " + l_loss + " and percent is " + l_percent + "%"
+      );
+      lossColorHandler();
+    } else if (current > initial) {
+      var profit = Number(current - initial) * quantity;
+      var p_profit = profit.toFixed(2);
+      var profitPercent = (profit / initial) * 100;
+      var p_percent = profitPercent.toFixed(2);
+      outputHandler(
+        "absolute profit is " + p_profit + " and percent is " + p_percent + "%"
+      );
+      profitColorHandler();
+    } else {
+      outputHandler("INVALID INPUT!");
+      outputBox.style.color = "red";
     }
-    else {
-        if ( initial > current ) {
-            const loss = ( initial - current ) * quantity;
-            const lossPercent = (loss / initial)*100;
-            outputHandler('loss is ' + loss + ' and percent is ' + lossPercent + '%');
-            outputBox.style.color = "red";
-        }
-        else if ( current > initial ) {
-            const profit = Number( current - initial ) * quantity;
-            const profitPercent = (profit / initial)*100;
-            outputHandler('profit is ' + profit + ' and percent is ' + profitPercent + '%');
-            outputBox.style.color = "green";
-        }
-        else {
-            outputHandler('INVALID INPUT!');
-            outputBox.style.color = "red";
-        }
-    }
+  }
 }
 
 function outputHandler(message) {
-    document.querySelector('#outputBox').innerHTML = message;
+  document.querySelector("#outputBox").innerHTML = message;
+}
+
+function profitColorHandler() {
+  outputBox.style.color = "green";
+}
+
+function lossColorHandler() {
+  outputBox.style.color = "red";
 }
